d6d7c1ef26209a6e81821f2dea0b63cc0d6d48c0
Cleberson Siqueira <cleberson.siqueira17@gmail.com>
2020-07-22 11:11:42 -0300

    Tratando erros por excecao

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -8,18 +8,26 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(2, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(6, 5));
-
-
-            Tela.imprimirTabuleiro(tab);
-
-
+                tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(2, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(6, 9));
+                
 
 
+                Tela.imprimirTabuleiro(tab);
 
+
+
+
+
+                
+            }
+            catch (TabuleiroException e){
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
