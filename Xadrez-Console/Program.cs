26b611229249c12abca56c37e62f10ea48e5a521
Cleberson Siqueira <cleberson.siqueira17@gmail.com>
2020-07-22 10:51:55 -0300

    Instanciando o tabuleiro na tela e comecando a criar as pecas

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace Xadrez_Console
 {
@@ -8,6 +9,17 @@ namespace Xadrez_Console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(2, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(6, 5));
+
+
+            Tela.imprimirTabuleiro(tab);
+
+
+
+
+
             Console.ReadLine();
         }
     }
